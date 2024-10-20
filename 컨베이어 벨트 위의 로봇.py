a8a0d9c8ad37a1c7c6c7615c416c73778986ee49
Factories-git <official.factories@gmail.com>
2024-10-20 19:22:46 +0900

    Changes

--- 컨베이어 벨트 위의 로봇.py
+++ 컨베이어 벨트 위의 로봇.py
@@ -7,44 +7,44 @@ from collections import deque
 n, k = map(int, input().split())
 conveyor = deque(map(int, input().split()))
 stage = 0
-robot = []
-
+robot = deque([0] * n * 2)
+pass_ = set()
 
 def is_finish():
     if conveyor.count(0) >= k:
         return True
     return False
 
+def clear_():
+    for i in range(n-1,len(robot)):
+        robot[i] = 0
 
 while True:
-    stage += 1 # 단계 상승
+    stage += 1  # 단계 상승
     #1번 행동
     # 컨베이어 벨트 회전
     conveyor.appendleft(conveyor.pop())
+    #로봇도 회전
+    robot.appendleft(robot.pop())
+    print(1,robot, conveyor)
     #회전 했으니 로봇들의 좌표 상승
-    for i in range(len(robot)):
-        robot[i] += 1
-
-    print(conveyor, robot)
-    #2번 행동
-    # 만약 로봇이 하나라도 있으면
-    if robot:
-        for i in range(n): #n번 반복
-            if i in robot: #지금 위치가 로봇이 있는 위치라면
-                #이동할 수 있는 지 확인.
-                if i + 1 not in robot and conveyor[i + 1] >= 1:
-                    conveyor[i + 1] -= 1 #이동 했으니 내구도 하락
-    #로봇 위치 상승
-    for i in range(len(robot)):
-        robot[i] += 1
-
-    print(conveyor, robot)
+    clear_() #로봇 내리기
+    for i in range(n-1):
+        if i not in pass_:
+            if robot[i] == 1:
+                print(i)
+                if robot[i+1] == 0 and conveyor[i+1] >= 1:
+                    robot[i+1],robot[i] = robot[i], robot[i+1]
+                    conveyor[i+1] -= 1
+                    pass_.add(i+1)
+    print(2,robot, conveyor)
+    clear_()
     #새로운 로봇을 올림
-    if conveyor[0] != 0: #만약 올리는 위치에 올릴 수 있다면
-        conveyor[0] -= 1 #내구도 하락
-        robot.append(0) #로봇의 좌표 추가
-    print(conveyor, robot)
-    if is_finish(): #만약 0의 개수가 k개 이상이면
-        break #종료
+    if conveyor[0] > 0:  #만약 올리는 위치에 올릴 수 있다면
+        conveyor[0] -= 1  #내구도 하락
+        robot[0] = 1
+    print(3,robot, conveyor)
+    if is_finish():  #만약 0의 개수가 k개 이상이면
+        break  #종료
 
 print(stage)
