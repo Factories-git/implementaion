8bb173d4978a8a31b8be81d4584d7987534bc8fc
Factories-git <official.factories@gmail.com>
2024-12-15 09:34:35 +0900

    Changes

--- No title.py
+++ No title.py
@@ -9,10 +9,17 @@ print('? 1 * 2')
 sys.stdout.flush()
 res = input().strip()
 if res == '+':
-    signs[1] = '+'
-else:
-    signs[1] = '-'
-for i in range(2,n+1):
+    print('? 1 + 2')
+    sys.stdout.flush()
+    res = input().strip()
+    if res == '+':
+        signs[1] = '+'
+        signs[2] = '+'
+    else:
+        signs[1] = '-'
+        signs[2] = '-'
+
+for i in range(3,n+1):
     print(f'? {i-1} * {i}')
     sys.stdout.flush()
     res = input().strip()
